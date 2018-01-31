9a8043d1f1de3cebd23f363ecc50bdc61b79d0f3
Fryanme <hf4@illinois.edu>
2018-01-30 18:36:09 -0600

    maze

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,6 +34,22 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
+            while ( maze.isFinished() == false) {
+                if (maze.canMove()){
+                    maze.move();
+                    maze.turnRight();
+                }
+                if (maze.canMove() == false) {
+                    maze.turnLeft();
+                    if ( maze.canMove() == false) {
+                        maze.turnLeft();
+                        if ( maze.canMove() == false ) {
+                            maze.turnLeft();
+                        }
+                    }
+                }
+            }
+
             // Implement your maze solving algorithm here
         }
 
